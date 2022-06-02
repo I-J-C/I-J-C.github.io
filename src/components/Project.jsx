1e92e8cf7b37f8504e978c8eb9bf5b28a8fd17f9
Jessie Collado <ilka.collado@gmail.com>
2022-06-01 20:52:21 -0400

    repo and deployed links online

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -7,10 +7,14 @@ const Project = ({ title, description, tech, repoLink, deployedLink }) => {
             <h3>{title}</h3>
             <p>{description}</p>
             <p>Tech used: {tech}</p>
+            <a href={deployedLink} target="_blank" rel="noreferrer">
             <button>Deployed Link
                 {/* deployedLink */}
             </button>
+            </a>
+            <a href={repoLink} target="_blank" rel="noreferrer">
             <button>Repository Link</button>
+            </a>
         </div> 
     )
 }
--- src/components/ProjectContainer.jsx
+++ src/components/ProjectContainer.jsx
@@ -1,11 +1,41 @@
 import Project from "./Project";
-import projects from '../projects';
+
 
 const ProjectContainer = () => {
-
+    const projects = [
+        {
+            title: "BlackJack",
+            description: "A Blackjack React.js app using the Deck of Cards API",
+            tech: "React.js, Axios, HTML/CSS",
+            repoLink: "https://github.com/I-J-C/blackjack",
+            deployedLink: "https://ijc-blackjack.surge.sh/"
+        },
+        {
+            title: "Minesweeper",
+            description: "A normal minesweeper web app functional on mobile",
+            tech: "HTML/CSS, Javascript",
+            repoLink: "https://github.com/I-J-C/minesweeper",
+            deployedLink: "https://i-j-c.github.io/minesweeper/"
+        },
+        {
+            title: "Unit Converter",
+            description: "Android App that converts units. Takes a base unit for each category (Distance, Time, Volume, etc) and converts to other units in that category.",
+            tech: "XML, Java, Android Studio",
+            repoLink: "https://github.com/I-J-C/Master-Unit-Converter",
+            deployedLink: "N/A"
+        }
+    ]
+    
+    // {
+    //     title: "",
+    //     description: "",
+    //     tech: "",
+    //     repoLink: "",
+    //     deployedLink: ""
+    // }
     return (
         <div className="projects">
-            {projects?.map((project, index) => {
+            {projects?.map((project, index) => (
                 <Project class="project"
                 title={project.title}
                 description={project.description}
@@ -14,7 +44,7 @@ const ProjectContainer = () => {
                 deployedLink={project.deployedLink}
                 key={index}
                 />
-            })}
+            ))}
         </div>
     )
 }
